7b4a32555ab2966aa3a45ea1e8c61730639bbdce
Fede Lacabaratz <lacarq@gmail.com>
2020-10-07 15:25:52 +0200

    add testing to previous exercises

---
+++ 03-counter-app/src/tests/base/02-template-string.test.js
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom';
+import { getSaludo } from '../../base/02-template-string';
+
+describe('Pruebas en 02-template-string.js', () => {
+    
+    test('getSaludo debe de retornar "Hola Federico"', () => {
+        
+        const nombre = 'Federico';
+
+        const saludo = getSaludo(nombre);
+
+        expect(saludo).toBe('Hola ' + nombre + '!');
+
+    });
+
+    // getSaludo debe de retornar "Hola Carlos!" si no hay argumento en el nombre
+    test('getSaludo debe de retornar "Hola Carlos!" si no hay argumentos en el nombre', () => {
+        
+        const saludo = getSaludo();
+
+        expect(saludo).toBe('Hola Carlos!');
+    });
+})
